4abae577208ac0055233720c835b74a3f3189427
Emrah T <33878173+EmrhT@users.noreply.github.com>
2023-05-13 22:24:51 +0300

    Update HelloWorldResource.java

--- src/main/java/jayray/net/hello/HelloWorldResource.java
+++ src/main/java/jayray/net/hello/HelloWorldResource.java
@@ -18,7 +18,7 @@ public class HelloWorldResource {
 		logger.info("sample info message");
 		logger.warn("sample warning message");
 		logger.error("sample error message");
-		return "hello world!!";
+		return "HELLO WORLD!!";
 	}
 
 }
